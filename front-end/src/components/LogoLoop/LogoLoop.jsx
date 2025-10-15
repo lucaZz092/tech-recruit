79173fe779e26ebc5acaa05baebebe3a95b02fe2
Lucas Mendonca <lucacampeao2013@gmail.com>
2025-10-15 17:34:24 -0300

    fix: corrige import do CSS do LogoLoop para Logoloop.css

--- front-end/src/components/LogoLoop/LogoLoop.jsx
+++ front-end/src/components/LogoLoop/LogoLoop.jsx
@@ -1,5 +1,5 @@
 import { useCallback, useEffect, useMemo, useRef, useState, memo } from 'react';
-import './LogoLoop.css';
+import './Logoloop.css';
 
 const ANIMATION_CONFIG = {
   SMOOTH_TAU: 0.25,
